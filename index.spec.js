2c7c73e8cef33711ed6132191fb42f5ed0f2042b
monu-simon <monusimonm@gmail.com>
2023-04-12 22:37:19 +0530

    before Each implementatiion

--- index.spec.js
+++ index.spec.js
@@ -1,22 +1,25 @@
-describe('calculator',() => {
+
+describe('calculator',() => {  
+
+    let c;
+    beforeEach(function() {
+        c= new Calculator();
+    })
+
     it('adds the number to the total',() => {
-        const c = new Calculator();
         c.add(5); 
         expect(c.total).toBe(5);
     })
 
     it('substracts the number from the total',() => {
-        const c = new Calculator();
         c.substract(1);
         expect(c.total).toBe(-1)
     })
     it('multiple the number to the total',() => {
-        const c = new Calculator();
         c.multiply(4);
         expect(c.total).toBe(0)
     })
     it('divides the number to the total',() => {
-        const c = new Calculator();
         c.divide(12);
         expect(c.total).toBe(0)
     })
